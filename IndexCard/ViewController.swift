9e5d821ff0a2c4dbbf462b8570ba24c0907eb47b
hyewone <won5854@naver.com>
2022-02-16 14:43:25 +0900

    Initial Commit

---
+++ IndexCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IndexCard
+//
+//  Created by 혜니 on 2022/02/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
