bf4db997c55c9fb5914813c2abe78b3c3b3b0eb6
hyewone <won5854@naver.com>
2022-02-16 16:58:11 +0900

    main commit

--- IndexCard/ViewController.swift
+++ IndexCard/ViewController.swift
@@ -13,7 +13,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // [메인] 파파고 번역기 버튼
 
-
+    // [메인] 인덱스 카드 관리 버튼
+//    @IBAction func btn_manageCard(_ sender: UIButton) {
+//    }
+//
+//    // [메인] 레게장 버튼
+//    @IBAction func btn_regeJeanPage(_ sender: UIButton) {
+//    }
+//
+//    // [메인] 박효신 버튼
+//    @IBAction func btn_parkHyoShin(_ sender: UIButton) {
+//    }
+//
+//    // [1. 파파고 번역기] 내 뒤로가기 버튼
+//    @IBAction func btn_backInPapago(_ sender: UIButton) {
+//    }
 }
 
